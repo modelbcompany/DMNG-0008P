7e48f12e24ee535ee76ba47eabbebdf8151809a4
Lexus Drumgold <lex@flexdevelopment.llc>
2020-05-07 23:43:19 -0400

    👌🏾 IMPROVE: Remove unused themes

--- app/public/wp-content/themes/grate-master/templates/comment-count.php
+++
@@ -1,3 +0,0 @@
-<p class="footer-comment-count">
-	<?php comments_number( __( '<span>No</span> Comments', 'grate' ), __( '<span>One</span> Comment', 'grate' ), __( '<span>%</span> Comments', 'grate' ) );?>
-</p>
--- app/public/wp-content/themes/grate-master/templates/post-navigation.php
+++
@@ -1,5 +0,0 @@
-<div class="post-navigation">
-
-	<?php grate_page_navi(); ?>
-
-</div>
