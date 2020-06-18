af5bd40c65e37f2a6eaef60da760f002316bd17c
Lexus Drumgold <lexus.drumgold@modelb.com>
2020-06-18 18:59:52 -0400

    🐛 FIX: deployments

--- app/public/.wpengine-conf/wp-config-site.php
+++
@@ -1,5 +0,0 @@
-<?php
-# GENERATED AUTOMATICALLY ON 2020-06-15 16:21:18.692416 -0400 EDT m=+260.929714407
-$table_prefix = "wp_";
-$wpengine_primary_original_domain = "woodmontdev.wpengine.com";
-$wpengine_primary_original_ip_address = "35.185.40.226";
